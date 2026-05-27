75361433860374ba46d90c3b38f86f147c1e391c
ibuler <ibuler@qq.com>
2026-05-27 18:32:09 +0800

    perf: disable some lookup plugin

--- lib/ansible/template/__init__.py
+++ lib/ansible/template/__init__.py
@@ -75,6 +75,7 @@ JINJA2_BEGIN_TOKENS = frozenset(('variable_begin', 'block_begin', 'comment_begin
 JINJA2_END_TOKENS = frozenset(('variable_end', 'block_end', 'comment_end', 'raw_end'))
 
 RANGE_TYPE = type(range(0))
+DISABLED_LOOKUPS = ('file', 'pipe', 'env', )
 
 
 def generate_ansible_template_vars(path, fullpath=None, dest_path=None):
@@ -833,6 +834,9 @@ class Templar:
         return self._lookup(name, *args, **kwargs)
 
     def _lookup(self, name, /, *args, **kwargs):
+        if name in DISABLED_LOOKUPS:
+            raise AnsibleError("The lookup `%s` is disabled from templating" % name)
+
         instance = lookup_loader.get(name, loader=self._loader, templar=self)
 
         if instance is None:
