61919e8306b946ffd265425d1f6d72af081eb46e
Crane.z <1481445951@qq.com>
2026-09-24 16:53:44 +0800

    fix: block lookup restriction bypasses

--- lib/ansible/template/__init__.py
+++ lib/ansible/template/__init__.py
@@ -64,7 +64,7 @@ from ansible.utils.unsafe_proxy import to_unsafe_text, wrap_var, AnsibleUnsafeTe
 display = Display()
 
 
-__all__ = ['Templar', 'generate_ansible_template_vars']
+__all__ = ['Templar', 'generate_ansible_template_vars', 'is_disabled_lookup']
 
 # Primitive Types which we don't want Jinja to convert to strings.
 NON_TEMPLATED_TYPES = (bool, Number)
@@ -75,7 +75,16 @@ JINJA2_BEGIN_TOKENS = frozenset(('variable_begin', 'block_begin', 'comment_begin
 JINJA2_END_TOKENS = frozenset(('variable_end', 'block_end', 'comment_end', 'raw_end'))
 
 RANGE_TYPE = type(range(0))
-DISABLED_LOOKUPS = ('file', 'pipe', 'env', )
+DISABLED_LOOKUPS = frozenset(('file', 'pipe', 'env', 'password', 'lines'))
+
+
+def is_disabled_lookup(name):
+    if not isinstance(name, string_types):
+        return False
+
+    name = name.strip().lower()
+    short_name = name.rsplit('.', 1)[-1]
+    return short_name in DISABLED_LOOKUPS
 
 
 def generate_ansible_template_vars(path, fullpath=None, dest_path=None):
@@ -834,7 +843,7 @@ class Templar:
         return self._lookup(name, *args, **kwargs)
 
     def _lookup(self, name, /, *args, **kwargs):
-        if name in DISABLED_LOOKUPS:
+        if is_disabled_lookup(name):
             raise AnsibleError("The lookup `%s` is disabled from templating" % name)
 
         instance = lookup_loader.get(name, loader=self._loader, templar=self)
